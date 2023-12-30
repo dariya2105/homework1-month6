ccdd31cc394cf15a8b78e463a89453c2fa207444
dariya2105 <kalmyrzaevvaa@gmail.com>
2023-12-30 13:28:14 +0600

    Initial Commit

---
+++ homework 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework 1
+//
+//  Created by Dariya on 30/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
